831d0a19e831c114312f53916a52fb15ff9d2ea7
Gokulkrishhh <44408044+Gokulkrishhh@users.noreply.github.com>
2018-11-09 19:27:35 +0530

    updated the code

--- Lampserver.sh
+++ Lampserver.sh
@@ -1,10 +1,14 @@
 sudo yum update -y
+sleep 15
 sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+sleep 20
 sudo yum install -y httpd mariadb-server
+sleep 10
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo systemctl is-enabled httpd
 echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
 sudo systemctl start mariadb
+sleep 5
 sudo systemctl enable  mariadb
 sudo systemctl is-enabled  mariadb
